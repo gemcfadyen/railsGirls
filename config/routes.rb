504f0bc70f187c22ebe4847ec817ba28667b5336
gemcfadyen <gemcfadyen@hotmail.co.uk>
2014-10-18 16:08:49 +0100

    Added rails_12factor gem and updated Gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Railsgirls::Application.routes.draw do
   get "pages/info"
   resources :ideas
 
+  root :to => redirect('/ideas')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,5 +58,4 @@ Railsgirls::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root :to => redirect('/ideas')
 end
